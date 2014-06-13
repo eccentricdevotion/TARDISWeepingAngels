13e6db81bacf7555ec7f78de6aaff6ebbce6298c
eccentric devotion <eccentricdevotion@gmail.com>
2014-06-14 00:04:21 +1200

    Fix death messages

--- src/main/java/me/eccentric_nz/tardisweepingangels/TARDISWeepingAngelsPlayerDeath.java
+++ src/main/java/me/eccentric_nz/tardisweepingangels/TARDISWeepingAngelsPlayerDeath.java
@@ -5,9 +5,12 @@ package me.eccentric_nz.tardisweepingangels;
 
 import me.libraryaddict.disguise.DisguiseAPI;
 import org.bukkit.Material;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.PigZombie;
+import org.bukkit.entity.Projectile;
 import org.bukkit.entity.Skeleton;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
@@ -18,7 +21,7 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.PotionEffectType;
+import org.bukkit.projectiles.ProjectileSource;
 
 /**
  *
@@ -35,58 +38,67 @@ public class TARDISWeepingAngelsPlayerDeath implements Listener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         EntityDamageEvent damage = event.getEntity().getLastDamageCause();
-        if (damage != null && damage.getCause().equals(DamageCause.ENTITY_ATTACK)) {
+        if (damage != null) {
             Entity attacker = (((EntityDamageByEntityEvent) damage).getDamager());
-            if (attacker instanceof Zombie) {
+            if (damage.getCause().equals(DamageCause.ENTITY_ATTACK)) {
+                if (attacker instanceof Zombie) {
+                    EntityEquipment ee = ((LivingEntity) attacker).getEquipment();
+                    ItemStack is = ee.getHelmet();
+                    if (is != null && is.hasItemMeta() && is.getItemMeta().hasDisplayName()) {
+                        String dn = is.getItemMeta().getDisplayName();
+                        if (dn.startsWith("Cyberman")) {
+                            String what_happened = (plugin.getConfig().getBoolean("cybermen.can_upgrade")) ? "upgraded" : "slain";
+                            String name = event.getEntity().getName();
+                            event.setDeathMessage(name + " was " + what_happened + " by a Cyberman");
+                            return;
+                        }
+                        if (dn.startsWith("Empty Child")) {
+                            String name = event.getEntity().getName();
+                            event.setDeathMessage(name + " was slain by an Empty Child");
+                            return;
+                        }
+                        if (dn.startsWith("Zygon")) {
+                            String name = event.getEntity().getName();
+                            event.setDeathMessage(name + " was slain by a Zygon");
+                            return;
+                        }
+                    }
+                }
+                if (attacker instanceof PigZombie) {
+                    EntityEquipment ee = ((LivingEntity) attacker).getEquipment();
+                    if (ee.getHelmet().getType().equals(Material.IRON_HELMET)) {
+                        String name = event.getEntity().getName();
+                        event.setDeathMessage(name + " was slain by an Ice Warrior");
+                        return;
+                    }
+                }
+            }
+            if (attacker.getType().equals(EntityType.ARROW)) {
+                Projectile arrow = (Arrow) attacker;
+                ProjectileSource source = arrow.getShooter();
+                if (source instanceof Skeleton) {
+                    Skeleton skeleton = (Skeleton) source;
+                    if (DisguiseAPI.isDisguised(skeleton)) {
+                        String name = event.getEntity().getName();
+                        event.setDeathMessage(name + " was slain by a Dalek");
+                        return;
+                    }
+                }
                 EntityEquipment ee = ((LivingEntity) attacker).getEquipment();
                 ItemStack is = ee.getHelmet();
                 if (is != null && is.hasItemMeta() && is.getItemMeta().hasDisplayName()) {
                     String dn = is.getItemMeta().getDisplayName();
-                    if (dn.startsWith("Cyberman")) {
-                        String what_happened = (plugin.getConfig().getBoolean("cybermen.can_upgrade")) ? "upgraded" : "slain";
+                    if (ee.getHelmet().getType().equals(Material.WATER_LILY)) {
                         String name = event.getEntity().getName();
-                        event.setDeathMessage(name + " was " + what_happened + " by a Cyberman");
+                        event.setDeathMessage(name + " was slain by a Weeping Angel");
                         return;
                     }
-                    if (dn.startsWith("Empty Child")) {
-                        String name = event.getEntity().getName();
-                        event.setDeathMessage(name + " was slain by an Empty Child");
-                        return;
+                    if (ee.getHelmet().getType().equals(Material.GOLD_HELMET)) {
+                        if (is.hasItemMeta() && is.getItemMeta().hasDisplayName() && dn.startsWith("Silurian")) {
+                            String name = event.getEntity().getName();
+                            event.setDeathMessage(name + " was slain by a Silurian");
+                        }
                     }
-                    if (dn.startsWith("Zygon")) {
-                        String name = event.getEntity().getName();
-                        event.setDeathMessage(name + " was slain by a Zygon");
-                        return;
-                    }
-                }
-            }
-            if (attacker instanceof Skeleton) {
-                EntityEquipment ee = ((LivingEntity) attacker).getEquipment();
-                if (ee.getHelmet().getType().equals(Material.WATER_LILY)) {
-                    String name = event.getEntity().getName();
-                    event.setDeathMessage(name + " was slain by a Weeping Angel");
-                    return;
-                }
-                if (ee.getHelmet().getType().equals(Material.GOLD_HELMET)) {
-                    ItemStack is = ee.getHelmet();
-                    if (is.hasItemMeta() && is.getItemMeta().hasDisplayName() && is.getItemMeta().getDisplayName().startsWith("Silurian")) {
-                        String name = event.getEntity().getName();
-                        event.setDeathMessage(name + " was slain by a Silurian");
-                        return;
-                    }
-                }
-                Skeleton dalek = (Skeleton) event.getEntity();
-                if (dalek.hasPotionEffect(PotionEffectType.DAMAGE_RESISTANCE) && DisguiseAPI.isDisguised(dalek)) {
-                    String name = event.getEntity().getName();
-                    event.setDeathMessage(name + " was slain by a Dalek");
-                    return;
-                }
-            }
-            if (attacker instanceof PigZombie) {
-                EntityEquipment ee = ((LivingEntity) attacker).getEquipment();
-                if (ee.getHelmet().getType().equals(Material.IRON_HELMET)) {
-                    String name = event.getEntity().getName();
-                    event.setDeathMessage(name + " was slain by an Ice Warrior");
                 }
             }
         }
