5ed68b6d1d82d54b2acb2e44bdada7730ae1e416
eccentric devotion <eccentricdevotion@gmail.com>
2014-06-17 23:18:17 +1200

    Right-clicking Strax with an empty bucket fills it with Sontaran Lactic Fluid

--- src/main/java/me/eccentric_nz/tardisweepingangels/monsters/sontarans/Butler.java
+++ src/main/java/me/eccentric_nz/tardisweepingangels/monsters/sontarans/Butler.java
@@ -3,6 +3,10 @@
  */
 package me.eccentric_nz.tardisweepingangels.monsters.sontarans;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import me.eccentric_nz.tardisweepingangels.TARDISWeepingAngels;
 import me.eccentric_nz.tardisweepingangels.equip.MonsterEquipment;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
@@ -13,12 +17,23 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 /**
+ * The seemingly male Sontarans could be genespliced to produce milk. Strax was
+ * very proud that he could produce "magnificent quantities" of lactic fluid and
+ * offered to nurse Melody Pond.
  *
  * @author eccentric_nz
  */
 public class Butler implements Listener {
+
+    private final List<UUID> milkers = new ArrayList<UUID>();
+    private final TARDISWeepingAngels plugin;
+
+    public Butler(TARDISWeepingAngels plugin) {
+        this.plugin = plugin;
+    }
 
     @EventHandler
     public void onSontaranInteract(PlayerInteractEntityEvent event) {
@@ -44,6 +59,36 @@ public class Butler implements Listener {
                         }
                         // switch the armour to a butler uniform
                         new MonsterEquipment().setButlerEquipment(pz, false);
+                    }
+                    return;
+                }
+            }
+            if (ee.getHelmet().getType().equals(Material.CHAINMAIL_HELMET)) {
+                ItemStack h = ee.getHelmet();
+                if (h.hasItemMeta() && h.getItemMeta().hasDisplayName() && h.getItemMeta().getDisplayName().startsWith("Strax")) {
+                    Player p = event.getPlayer();
+                    final UUID uuid = p.getUniqueId();
+                    ItemStack is = p.getItemInHand();
+                    if (is.getType().equals(Material.BUCKET)) {
+                        if (!milkers.contains(uuid)) {
+                            milkers.add(uuid);
+                            p.playSound(pz.getLocation(), "milk", 1.0f, 1.0f);
+                            ItemStack milk = new ItemStack(Material.MILK_BUCKET);
+                            ItemMeta m = milk.getItemMeta();
+                            m.setDisplayName("Sontaran Lactic Fluid");
+                            milk.setItemMeta(m);
+                            p.setItemInHand(milk);
+                            plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+                                @Override
+                                public void run() {
+                                    milkers.remove(uuid);
+                                }
+                            }, 3000L);
+                        } else {
+                            p.sendMessage(plugin.pluginName + "Strax is not lactating right now, try again later.");
+                        }
+                    } else {
+                        p.playSound(pz.getLocation(), "strax", 1.0f, 1.0f);
                     }
                 }
             }
