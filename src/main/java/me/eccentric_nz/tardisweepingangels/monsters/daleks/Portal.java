b90d259b7a776f9ca4e7672454bae692460da790
eccentricdevotion <eccentricdevotion@gmail.com>
2015-01-31 16:11:17 +1300

    Redsiguise Daleks if they go through a Nether Portal

---
+++ src/main/java/me/eccentric_nz/tardisweepingangels/monsters/daleks/Portal.java
@@ -0,0 +1,50 @@
+/*
+ *  Copyright 2015 eccentric_nz.
+ */
+package me.eccentric_nz.tardisweepingangels.monsters.daleks;
+
+import me.eccentric_nz.tardisweepingangels.TARDISWeepingAngels;
+import me.libraryaddict.disguise.DisguiseAPI;
+import me.libraryaddict.disguise.disguisetypes.DisguiseType;
+import me.libraryaddict.disguise.disguisetypes.MobDisguise;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Skeleton;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPortalExitEvent;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ *
+ * @author eccentric_nz
+ */
+public class Portal implements Listener {
+
+    private final TARDISWeepingAngels plugin;
+
+    public Portal(TARDISWeepingAngels plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void dalekExitPortal(EntityPortalExitEvent event) {
+        Entity e = event.getEntity();
+        if (e.getType().equals(EntityType.SKELETON)) {
+            final Skeleton skeleton = (Skeleton) e;
+            final EntityEquipment ee = skeleton.getEquipment();
+            final ItemStack is = ee.getHelmet();
+            plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+                @Override
+                public void run() {
+                    if (is.getType().equals(Material.VINE) && !DisguiseAPI.isDisguised(skeleton)) {
+                        MobDisguise mobDisguise = new MobDisguise(DisguiseType.SNOWMAN);
+                        DisguiseAPI.disguiseToAll(skeleton, mobDisguise);
+                    }
+                }
+            }, 5L);
+        }
+    }
+}
