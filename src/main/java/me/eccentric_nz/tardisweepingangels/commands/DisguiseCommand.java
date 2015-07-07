0db4eb775d4ccce73370000de09b7ddcac982200
eccentricdevotion <eccentricdevotion@gmail.com>
2015-07-07 19:12:17 +1200

    Implement use of `/twad` command from the console
    Need to supply a player UUID

--- src/main/java/me/eccentric_nz/tardisweepingangels/commands/DisguiseCommand.java
+++ src/main/java/me/eccentric_nz/tardisweepingangels/commands/DisguiseCommand.java
@@ -1,11 +1,13 @@
 package me.eccentric_nz.tardisweepingangels.commands;
 
+import java.util.UUID;
 import me.eccentric_nz.tardisweepingangels.TARDISWeepingAngels;
 import me.eccentric_nz.tardisweepingangels.equip.MonsterEquipment;
 import me.eccentric_nz.tardisweepingangels.utils.Monster;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.PlayerInventory;
 
@@ -40,8 +42,17 @@ public class DisguiseCommand implements CommandExecutor {
             if (sender instanceof Player) {
                 player = (Player) sender;
             }
+            if (sender instanceof ConsoleCommandSender) {
+                // check argument length
+                if (args.length < 3) {
+                    sender.sendMessage(plugin.pluginName + "You must supply a player UUID when using this command from the console!");
+                    return true;
+                }
+                UUID uuid = UUID.fromString(args[2]);
+                player = plugin.getServer().getPlayer(uuid);
+            }
             if (player == null) {
-                sender.sendMessage(plugin.pluginName + "Command can only be used by a player!");
+                sender.sendMessage(plugin.pluginName + "Command can only be used by a player, or a player UUID must be supplied!");
                 return true;
             }
             if (!args[1].equalsIgnoreCase("on") && !args[1].equalsIgnoreCase("off")) {
