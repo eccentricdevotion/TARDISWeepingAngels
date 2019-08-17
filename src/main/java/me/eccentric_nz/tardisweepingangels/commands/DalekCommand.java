a5ead1ada9b35dbdc0bf278b44c1bb64e16b66f5
eccentricdevotion <eccentricdevotion@gmail.com>
2019-08-18 00:28:42 +1200

    Add missing silent section to default config

--- src/main/java/me/eccentric_nz/tardisweepingangels/commands/DalekCommand.java
+++ src/main/java/me/eccentric_nz/tardisweepingangels/commands/DalekCommand.java
@@ -2,17 +2,13 @@ package me.eccentric_nz.tardisweepingangels.commands;
 
 import me.eccentric_nz.tardisweepingangels.TARDISWeepingAngels;
 import me.eccentric_nz.tardisweepingangels.monsters.daleks.ReDisguise;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import java.util.Set;
-
 public class DalekCommand implements CommandExecutor {
 
     private final TARDISWeepingAngels plugin;
-    private final Set<Material> trans = null;
 
     public DalekCommand(TARDISWeepingAngels plugin) {
         this.plugin = plugin;
