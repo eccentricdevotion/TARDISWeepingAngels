a85f595456921cd0cebbcc2694687399a42b7cc9
eccentricdevotion <eccentricdevotion@gmail.com>
2020-01-03 21:21:20 +1300

    Add display names to Ood items

--- src/main/java/me/eccentric_nz/tardisweepingangels/commands/OodCommand.java
+++ src/main/java/me/eccentric_nz/tardisweepingangels/commands/OodCommand.java
@@ -3,8 +3,7 @@ package me.eccentric_nz.tardisweepingangels.commands;
 import com.google.common.collect.ImmutableList;
 import me.eccentric_nz.tardisweepingangels.TARDISWeepingAngels;
 import me.eccentric_nz.tardisweepingangels.monsters.ood.OodWalkRunnable;
-import me.eccentric_nz.tardisweepingangels.monsters.weeping_angels.Blink;
-import me.eccentric_nz.tardisweepingangels.utils.Vector3D;
+import me.eccentric_nz.tardisweepingangels.utils.ArmourStandFinder;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -64,22 +63,25 @@ public class OodCommand implements CommandExecutor {
                 Entity entity = block.getLocation().getWorld().spawnEntity(location, EntityType.ARMOR_STAND);
                 ArmorStand armorStand = (ArmorStand) entity;
                 armorStand.getPersistentDataContainer().set(TARDISWeepingAngels.OOD, PersistentDataType.INTEGER, 0);
-                armorStand.getPersistentDataContainer().set(TARDISWeepingAngels.OOD_UUID, TARDISWeepingAngels.PersistentDataTypeUUID, uuid);
+                armorStand.getPersistentDataContainer().set(TARDISWeepingAngels.OWNER_UUID, TARDISWeepingAngels.PersistentDataTypeUUID, uuid);
                 ItemStack head = new ItemStack(Material.ROTTEN_FLESH);
                 ItemMeta headMeta = head.getItemMeta();
+                headMeta.setDisplayName("Ood Head");
                 headMeta.setCustomModelData(2 + colour);
                 head.setItemMeta(headMeta);
                 ItemStack arm = new ItemStack(Material.ROTTEN_FLESH);
                 ItemMeta armMeta = arm.getItemMeta();
+                armMeta.setDisplayName("Ood Arm");
                 armMeta.setCustomModelData(3 + colour);
                 arm.setItemMeta(armMeta);
-                ItemStack ball_arm = new ItemStack(Material.ROTTEN_FLESH);
-                ItemMeta ballMeta = ball_arm.getItemMeta();
-                ballMeta.setCustomModelData(4 + colour);
-                ball_arm.setItemMeta(ballMeta);
+                ItemStack brain_arm = new ItemStack(Material.ROTTEN_FLESH);
+                ItemMeta brainMeta = brain_arm.getItemMeta();
+                brainMeta.setDisplayName("Ood Brain Arm");
+                brainMeta.setCustomModelData(4 + colour);
+                brain_arm.setItemMeta(brainMeta);
                 EntityEquipment ee = armorStand.getEquipment();
                 armorStand.setHelmet(head);
-                ee.setItemInMainHand(ball_arm);
+                ee.setItemInMainHand(brain_arm);
                 armorStand.setRightArmPose(new EulerAngle(-1d, 0, 0.175d));
                 ee.setItemInOffHand(arm);
                 armorStand.setVisible(false);
@@ -93,7 +95,7 @@ public class OodCommand implements CommandExecutor {
                     player.sendMessage(plugin.pluginName + "An Ood is already following you!");
                     return true;
                 }
-                ArmorStand stand = getStand(player);
+                ArmorStand stand = ArmourStandFinder.getStand(player);
                 if (stand == null) {
                     player.sendMessage(plugin.pluginName + "You are not looking at an Ood!");
                     return true;
@@ -102,8 +104,8 @@ public class OodCommand implements CommandExecutor {
                     player.sendMessage(plugin.pluginName + "You are not looking at an Ood!");
                     return true;
                 }
-                if (stand.getPersistentDataContainer().has(TARDISWeepingAngels.OOD_UUID, TARDISWeepingAngels.PersistentDataTypeUUID)) {
-                    UUID oodId = stand.getPersistentDataContainer().get(TARDISWeepingAngels.OOD_UUID, TARDISWeepingAngels.PersistentDataTypeUUID);
+                if (stand.getPersistentDataContainer().has(TARDISWeepingAngels.OWNER_UUID, TARDISWeepingAngels.PersistentDataTypeUUID)) {
+                    UUID oodId = stand.getPersistentDataContainer().get(TARDISWeepingAngels.OWNER_UUID, TARDISWeepingAngels.PersistentDataTypeUUID);
                     if (oodId.equals(uuid)) {
                         double speed = (args.length == 2) ? Math.min(Double.parseDouble(args[1]) / 100.0d, 0.5d) : 0.15d;
                         int taskId = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new OodWalkRunnable(stand, speed, player), 2L, 2L);
@@ -128,7 +130,7 @@ public class OodCommand implements CommandExecutor {
                     player.sendMessage(plugin.pluginName + "Please tell your Ood to stay before removing it!");
                     return true;
                 }
-                ArmorStand stand = getStand(player);
+                ArmorStand stand = ArmourStandFinder.getStand(player);
                 if (stand == null) {
                     player.sendMessage(plugin.pluginName + "You are not looking at an Ood!");
                     return true;
@@ -137,8 +139,8 @@ public class OodCommand implements CommandExecutor {
                     player.sendMessage(plugin.pluginName + "You are not looking at an Ood!");
                     return true;
                 }
-                if (stand.getPersistentDataContainer().has(TARDISWeepingAngels.OOD_UUID, TARDISWeepingAngels.PersistentDataTypeUUID)) {
-                    UUID oodId = stand.getPersistentDataContainer().get(TARDISWeepingAngels.OOD_UUID, TARDISWeepingAngels.PersistentDataTypeUUID);
+                if (stand.getPersistentDataContainer().has(TARDISWeepingAngels.OWNER_UUID, TARDISWeepingAngels.PersistentDataTypeUUID)) {
+                    UUID oodId = stand.getPersistentDataContainer().get(TARDISWeepingAngels.OWNER_UUID, TARDISWeepingAngels.PersistentDataTypeUUID);
                     if (oodId.equals(uuid)) {
                         stand.remove();
                     } else {
@@ -149,27 +151,5 @@ public class OodCommand implements CommandExecutor {
             }
         }
         return true;
-    }
-
-    private ArmorStand getStand(Player player) {
-        ArmorStand stand = null;
-        // get the armour stand player is looking at
-        Location observerPos = player.getEyeLocation();
-        Vector3D observerDir = new Vector3D(observerPos.getDirection());
-        Vector3D observerStart = new Vector3D(observerPos);
-        Vector3D observerEnd = observerStart.add(observerDir.multiply(16));
-        // Get nearby entities
-        for (Entity target : player.getNearbyEntities(8.0d, 8.0d, 8.0d)) {
-            // Bounding box of the given player
-            Vector3D targetPos = new Vector3D(target.getLocation());
-            Vector3D minimum = targetPos.add(-0.5, 0, -0.5);
-            Vector3D maximum = targetPos.add(0.5, 1.67, 0.5);
-            if (target.getType().equals(EntityType.ARMOR_STAND) && Blink.hasIntersection(observerStart, observerEnd, minimum, maximum)) {
-                if (stand == null || stand.getLocation().distanceSquared(observerPos) > target.getLocation().distanceSquared(observerPos)) {
-                    return (ArmorStand) target;
-                }
-            }
-        }
-        return stand;
     }
 }
--- src/main/java/me/eccentric_nz/tardisweepingangels/monsters/ood/OodListener.java
+++ src/main/java/me/eccentric_nz/tardisweepingangels/monsters/ood/OodListener.java
@@ -18,7 +18,7 @@ public class OodListener implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL)
     public void onManipulate(PlayerArmorStandManipulateEvent event) {
-        if (event.getRightClicked().getPersistentDataContainer().has(TARDISWeepingAngels.OOD, PersistentDataType.INTEGER)) {
+        if (event.getRightClicked().getPersistentDataContainer().has(TARDISWeepingAngels.OWNER_UUID, TARDISWeepingAngels.PersistentDataTypeUUID)) {
             event.setCancelled(true);
         }
     }
@@ -27,14 +27,14 @@ public class OodListener implements Listener {
     public void onDamage(EntityDamageByEntityEvent event) {
         if (event.getEntity() instanceof ArmorStand && event.getDamager() instanceof Player) {
             ArmorStand stand = (ArmorStand) event.getEntity();
-            if (stand.getPersistentDataContainer().has(TARDISWeepingAngels.OOD, PersistentDataType.INTEGER) && stand.getPersistentDataContainer().has(TARDISWeepingAngels.OOD_UUID, TARDISWeepingAngels.PersistentDataTypeUUID)) {
+            if (stand.getPersistentDataContainer().has(TARDISWeepingAngels.OOD, PersistentDataType.INTEGER) && stand.getPersistentDataContainer().has(TARDISWeepingAngels.OWNER_UUID, TARDISWeepingAngels.PersistentDataTypeUUID)) {
                 event.setCancelled(true);
                 Player player = (Player) event.getDamager();
                 player.playSound(stand.getLocation(), "ood", 1.0f, 1.0f);
                 if (!player.hasPermission("tardisweepingangels.ood")) {
                     return;
                 }
-                UUID oodId = stand.getPersistentDataContainer().get(TARDISWeepingAngels.OOD_UUID, TARDISWeepingAngels.PersistentDataTypeUUID);
+                UUID oodId = stand.getPersistentDataContainer().get(TARDISWeepingAngels.OWNER_UUID, TARDISWeepingAngels.PersistentDataTypeUUID);
                 if (oodId.equals(player.getUniqueId())) {
                     ItemStack head = stand.getHelmet();
                     ItemMeta im = head.getItemMeta();
