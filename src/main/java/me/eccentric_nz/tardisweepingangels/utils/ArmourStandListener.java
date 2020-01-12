dde626f6710a18d34eaef8d02f026e68467f2511
eccentricdevotion <eccentricdevotion@gmail.com>
2020-01-13 11:27:00 +1300

    Prevent armour stand interaction for all Monster stands

---
+++ src/main/java/me/eccentric_nz/tardisweepingangels/utils/ArmourStandListener.java
@@ -0,0 +1,17 @@
+package me.eccentric_nz.tardisweepingangels.utils;
+
+import me.eccentric_nz.tardisweepingangels.TARDISWeepingAngels;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
+
+public class ArmourStandListener implements Listener {
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onManipulateFakeMonster(PlayerArmorStandManipulateEvent event) {
+        if (event.getRightClicked().getPersistentDataContainer().has(TARDISWeepingAngels.OWNER_UUID, TARDISWeepingAngels.PersistentDataTypeUUID)) {
+            event.setCancelled(true);
+        }
+    }
+}
