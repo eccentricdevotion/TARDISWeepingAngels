de33be51d0f3b625db72ef9c50bfdd9b599a98f3
eccentricdevotion <eccentricdevotion@gmail.com>
2020-01-17 12:09:15 +1300

    Check WorldGuard global configuration for explosion damage as well

--- src/main/java/me/eccentric_nz/tardisweepingangels/utils/WorldGuardChecker.java
+++ src/main/java/me/eccentric_nz/tardisweepingangels/utils/WorldGuardChecker.java
@@ -1,8 +1,11 @@
 package me.eccentric_nz.tardisweepingangels.utils;
 
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.bukkit.BukkitWorld;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.bukkit.BukkitWorldConfiguration;
+import com.sk89q.worldguard.config.ConfigurationManager;
 import com.sk89q.worldguard.internal.platform.WorldGuardPlatform;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.Flags;
@@ -30,6 +33,11 @@ public class WorldGuardChecker {
         Plugin p = Bukkit.getPluginManager().getPlugin("WorldGuard");
         if (p != null) {
             WorldGuardPlatform wg = WorldGuard.getInstance().getPlatform();
+            ConfigurationManager cfg = wg.getGlobalStateManager();
+            BukkitWorldConfiguration wcfg = (BukkitWorldConfiguration) cfg.get(BukkitAdapter.adapt(l.getWorld()));
+            if (wcfg.blockCreeperBlockDamage || wcfg.blockTNTBlockDamage) {
+                return false;
+            }
             BlockVector3 vector = BlockVector3.at(l.getX(), l.getY(), l.getZ());
             RegionManager rm = wg.getRegionContainer().get(new BukkitWorld(l.getWorld()));
             ApplicableRegionSet rs = rm.getApplicableRegions(vector);
