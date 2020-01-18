db796a8c6671add40f2c363fbbb8d0ac79a06bfd
eccentricdevotion <eccentricdevotion@gmail.com>
2020-01-18 16:23:32 +1300

    Make Judoon shoot snowballs

--- src/main/java/me/eccentric_nz/tardisweepingangels/monsters/judoon/JudoonGuardRunnable.java
+++ src/main/java/me/eccentric_nz/tardisweepingangels/monsters/judoon/JudoonGuardRunnable.java
@@ -2,11 +2,9 @@ package me.eccentric_nz.tardisweepingangels.monsters.judoon;
 
 import me.eccentric_nz.tardisweepingangels.TARDISWeepingAngels;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.Damageable;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Monster;
+import org.bukkit.entity.*;
 import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.util.Vector;
 
 import java.util.UUID;
 
@@ -33,6 +31,11 @@ public class JudoonGuardRunnable implements Runnable {
                                 if (ammo > 0 && health > 0) {
                                     damageable.damage(plugin.getConfig().getDouble("judoon.damage"), entity);
                                     entity.getWorld().playSound(entity.getLocation(), "judoon_fire", 1.0f, 1.0f);
+                                    Snowball snowball = ((LivingEntity) entity).launchProjectile(Snowball.class);
+                                    Vector direction = damageable.getLocation().toVector().subtract(entity.getLocation().toVector());
+                                    direction.normalize();
+                                    Vector bulletVelocity = direction.multiply(3.0d);
+                                    snowball.setVelocity(bulletVelocity);
                                     ammo -= 1;
                                     if (ammo >= 0) {
                                         ArmorStand stand = (ArmorStand) entity;
